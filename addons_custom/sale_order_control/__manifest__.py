0b4acbde1bb21cfd43eaf8ddea9d565939aa3504
Zakaria171 <zakaria.rabih93@gmail.com>
2026-04-07 02:10:25 +0100

    fear: add promo_active field to sale order form

--- addons_custom/sale_order_control/__manifest__.py
+++ addons_custom/sale_order_control/__manifest__.py
@@ -6,7 +6,9 @@
     'author': 'workflow_odoo',
     'license': 'LGPL-3',
     'depends': ['sale'],
-    'data': [],
+    'data': [
+        "views/sale_order_view.xml",
+    ],
     'installable': True,
     'application': False,
     'auto_install': False,
--- addons_custom/sale_order_control/models/sale_order.py
+++ addons_custom/sale_order_control/models/sale_order.py
@@ -2,3 +2,5 @@ from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    promo_active = fields.Boolean(string="Promo Active", default=False)
